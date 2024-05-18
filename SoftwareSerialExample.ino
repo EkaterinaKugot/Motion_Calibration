f838724e0060eee9e9c18e0d00d58bb5c7366ab7
NikitaBagulov <117837048+NikitaBagulov@users.noreply.github.com>
2024-05-18 11:37:34 +0800

    Update SoftwareSerialExample.ino

--- SoftwareSerialExample.ino
+++ SoftwareSerialExample.ino
@@ -11,10 +11,10 @@
 #define LEFT 'L'
 #define RIGHT 'R'
 #define CHANGE_STATE 'A'
-// #define RETURN_STATE 'X'
 #define MOVE 'P'
 
 #define CHANGE 'T'
+#define CHANGE_SP 'X'
 #define SECURE 'C'
 
 #include <SoftwareSerial.h>
@@ -35,20 +35,20 @@ void move(bool lforward, bool rforward, int lvelocity, int rvelocity){
   analogWrite(SPEED_LEFT, lvelocity);
 }
 
-void move_forward(int velocity){
-  move(true, true, velocity, velocity);
+void move_forward(int vel1, int vel2){
+  move(true, true, vel1, vel2);
 }
 
-void move_back(int velocity){
-  move(false, false, velocity, velocity);
+void move_back(int vel1, int vel2){
+  move(false, false, vel1, vel2);
 }
 
-void rotate_left(int velocity){
-  move(false, true, velocity, velocity);
+void rotate_left(int vel1, int vel2){
+  move(false, true, vel1, vel2);
 }
 
-void rotate_right(int velocity){
-  move(true, false, velocity, velocity);
+void rotate_right(int vel1, int vel2){
+  move(true, false, vel1, vel2);
 }
 
 void turn_right(int velocity){
@@ -76,7 +76,7 @@ char* stateToString(State st) {
   }
 }
 
-typedef void (*CommandFunction)(int);
+typedef void (*CommandFunction)(int, int);
 
 CommandFunction move_commands[4] = {
   move_forward,     
@@ -93,8 +93,13 @@ int const size = 4;
 char tmp_command_chars[size] = {'F', 'B', 'R', 'L'};
 char command_chars[size] = {'q', 'q', 'q', 'q'};
 
-bool is_change = false;
-int fixed_speed = 200;
+bool is_change = false; // DIRECTION
+
+bool is_added = false; // SPEED
+bool is_reduced = false; // SPEED
+int const size_sp = 2;
+int fixed_speed[size_sp] = {205, 205};
+int shift_speed = 10;
 
 void setup() {
   for(int i = 0; i<=7; i++)
@@ -119,7 +124,9 @@ void loop() {
     case DIRECTION:
       direction_calibration(command);
       break;
-    
+    case SPEED:
+      speed_balancing(command);
+      break;
   }
 
   if(mySerial.available()){
@@ -127,6 +134,69 @@ void loop() {
     Serial.println(command);
     select_state();
   }
+}
+
+void speed_balancing(char command){
+  switch(command){
+    case(CHANGE): // прибавляем скорость
+      if (!is_added){
+        if(prev_comm == RIGHT){
+          add_speed(fixed_speed, 1);
+        }else if (prev_comm == LEFT){
+          add_speed(fixed_speed, 0);
+        }
+        is_added = true;
+      }
+      break;
+    case(CHANGE_SP): // убавляем скорость
+      if (!is_reduced){
+        if(prev_comm == RIGHT){
+          reduce_speed(fixed_speed, 1);
+        }else if (prev_comm == LEFT){
+          reduce_speed(fixed_speed, 0);
+        }
+        is_reduced = true;
+      }
+      break;
+    default: // калибровка скорости
+      is_added = false;
+      is_reduced = false;
+      for (int i = 0; i < size; ++i) {
+        if (command == command_chars[i]) {
+          if (command != LEFT && command != RIGHT){ // право и лево для изменения скорости, вперед и назад - для проверки
+            move_commands[i](fixed_speed[0], fixed_speed[1]);
+          }
+          prev_comm = command;
+          return;
+        }
+      }
+      stop();
+      break;
+  }
+}
+
+void add_speed(int arr[], int idx){
+  if (arr[idx] + shift_speed <= 255){
+    arr[idx] += shift_speed;
+  }else{
+    Serial.println("Maximum speed! (255)");
+  }
+  Serial.print("Left: ");
+  Serial.println(arr[0]);
+  Serial.print("Right: ");
+  Serial.println(arr[1]);
+}
+
+void reduce_speed(int arr[], int idx){
+  if (arr[idx] - shift_speed >= 5){
+    arr[idx] -= shift_speed;
+  }else{
+    Serial.println("Minimum speed! (5)");
+  }
+  Serial.print("Left: ");
+  Serial.println(arr[0]);
+  Serial.print("Right: ");
+  Serial.println(arr[1]);
 }
 
 void direction_calibration(char command){
@@ -137,8 +207,8 @@ void direction_calibration(char command){
         is_change = true;
       }
       break;
-    case(SECURE): //закрепляем
-      for (int i = 0; i < 4; ++i) {
+    case(SECURE): // закрепляем
+      for (int i = 0; i < size; ++i) {
         if (prev_comm == tmp_command_chars[i]) {
           command_chars[i] = prev_comm;
           printArray(command_chars, size);
@@ -146,11 +216,11 @@ void direction_calibration(char command){
         }
       }
       break;
-    default:
+    default: // калибровка направления и движения
       is_change = false;
-      for (int i = 0; i < 4; ++i) {
+      for (int i = 0; i < size; ++i) {
         if (command == tmp_command_chars[i]) {
-          move_commands[i](fixed_speed);
+          move_commands[i](fixed_speed[0], fixed_speed[1]);
           prev_comm = command;
           return;
         }
@@ -206,9 +276,9 @@ void printArray(char arr[], int size) {
 }
 
 void executeCommand(char command){
-  for (int i = 0; i < 4; ++i) {
+  for (int i = 0; i < size; ++i) {
     if (command == command_chars[i]) {
-      move_commands[i](fixed_speed);
+      move_commands[i](fixed_speed[0], fixed_speed[1]);
       return;
     }
   }
